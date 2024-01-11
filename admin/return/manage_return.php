819dcfdf108af32022b81276f4911729d18ce56e
KarkiMilan <milankarki723@gmail.com>
2024-01-11 21:00:21 +0545

    fix : 0 and Negative values fixed

--- admin/return/manage_return.php
+++ admin/return/manage_return.php
@@ -79,7 +79,7 @@ if(isset($_GET['id'])){
                         <div class="col-md-3">
                             <div class="form-group">
                                 <label for="qty" class="control-label">Qty</label>
-                                <input type="number" step="any" class="form-control rounded-0" id="qty">
+                                <input type="number" step="any" class="form-control rounded-0" id="qty" min="1" required>
                             </div>
                         </div>
                         <div class="col-md-2 text-center">
